b3ee1772822c8f421da935481579089be453b907
keythkatz <github@jerl.im>
2016-04-02 03:24:49 +0800

    Quick and dirty fix user counts for room size >200

--- robinautovoter.user.js
+++ robinautovoter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Robin Autovoter
 // @namespace    http://jerl.im
-// @version      1.8
+// @version      1.9
 // @description  Autovotes via text on /r/robin
 // @author       /u/keythkatz
 // @match        https://www.reddit.com/robin*
@@ -16,9 +16,14 @@ function sendMessage(message){
 }
 
 setTimeout(function(){
+    var participants = $(".robin-room-participant").length;
+    var partiText = "";
+    if (participants == 200) partiText = 200 + " " + $(".robin-user-list-overflow-indicator").text();
+    else partiText = participants;
+    
     sendMessage("/vote grow");
-    if(Math.random() < 0.2) sendMessage("[Robin Autovoter 1.8] Autovoted grow! https://www.reddit.com/r/joinrobin/comments/4cx02w/better_working_automatic_grow_script/");
-    setTimeout(function(){sendMessage("[Robin Autovoter 1.8] " + $(".robin-room-participant").length + " in this room! " + $("span:contains('Voting will end')").first().text());}, 10000);
+    if(Math.random() < 0.2) sendMessage("[Robin Autovoter 1.9] Autovoted grow! https://www.reddit.com/r/joinrobin/comments/4cx02w/better_working_automatic_grow_script/");
+    setTimeout(function(){sendMessage("[Robin Autovoter 1.9] " + partiText + " in this room! " + $("span:contains('Voting will end')").first().text());}, 10000);
     setTimeout(function(){
         window.location.reload();
     }, 300000);
