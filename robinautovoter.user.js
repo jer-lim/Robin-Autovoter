0b5c523333b8d1c0cf769deaf1f6b994dedd3c90
keythkatz <github@jerl.im>
2016-04-02 02:11:30 +0800

    Update link to grow script

--- robinautovoter.user.js
+++ robinautovoter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Robin Autovoter
 // @namespace    http://jerl.im
-// @version      1.4
+// @version      1.5
 // @description  Autovotes via text on /r/robin
 // @author       /u/keythkatz
 // @match        https://www.reddit.com/robin*
@@ -17,8 +17,8 @@ function sendMessage(message){
 
 setTimeout(function(){
     sendMessage("/vote grow");
-    if(Math.random() < 0.2) sendMessage("[Robin Autovoter 1.4] Autovoted grow! https://www.reddit.com/r/joinrobin/comments/4cwk2s/automatic_grow_userscript_bot/d1lzfpu");
-    setTimeout(function(){sendMessage("[Robin Autovoter 1.4] " + $("span:contains('Voting will end')").text());}, 5000);
+    if(Math.random() < 0.2) sendMessage("[Robin Autovoter 1.5] Autovoted grow! https://www.reddit.com/r/joinrobin/comments/4cx02w/better_working_automatic_grow_script/");
+    setTimeout(function(){sendMessage("[Robin Autovoter 1.5] " + $("span:contains('Voting will end')").text());}, 5000);
     setTimeout(function(){
         window.location.reload();
     }, 300000);
