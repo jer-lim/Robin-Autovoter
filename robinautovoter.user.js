579680a7d60374dbed2f5ad551e1b41c86a3fa09
keythkatz <github@jerl.im>
2016-04-02 02:15:25 +0800

    Send timer status later

--- robinautovoter.user.js
+++ robinautovoter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Robin Autovoter
 // @namespace    http://jerl.im
-// @version      1.5
+// @version      1.6
 // @description  Autovotes via text on /r/robin
 // @author       /u/keythkatz
 // @match        https://www.reddit.com/robin*
@@ -17,8 +17,8 @@ function sendMessage(message){
 
 setTimeout(function(){
     sendMessage("/vote grow");
-    if(Math.random() < 0.2) sendMessage("[Robin Autovoter 1.5] Autovoted grow! https://www.reddit.com/r/joinrobin/comments/4cx02w/better_working_automatic_grow_script/");
-    setTimeout(function(){sendMessage("[Robin Autovoter 1.5] " + $("span:contains('Voting will end')").text());}, 5000);
+    if(Math.random() < 0.2) sendMessage("[Robin Autovoter 1.6] Autovoted grow! https://www.reddit.com/r/joinrobin/comments/4cx02w/better_working_automatic_grow_script/");
+    setTimeout(function(){sendMessage("[Robin Autovoter 1.6] " + $("span:contains('Voting will end')").text());}, 10000);
     setTimeout(function(){
         window.location.reload();
     }, 300000);
